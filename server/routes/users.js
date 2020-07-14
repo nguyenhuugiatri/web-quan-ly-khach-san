ce171c71ccd7be250d0417bfb1bcabf2f6b83ad9
huynhthaianhhd <huynhthaianhhd@gmail.com>
2020-07-14 15:27:48 +0700

    user list

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,5 +3,9 @@ const router = require('express').Router();
 router.get('/login',(req,res)=>{
     res.send('this is login');
 })
+router.get('/list',(req,res)=>{
+    res.send('this is login');
+})
+
 
 module.exports=router
--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import LoginPage from './containers/LoginPage';
 import HomePage from './containers/HomePage';
+import Users from './containers/Users';
+import CheckIn from './containers/CheckIn';
 function App() {
   return (
     <>
       <LoginPage />
       <HomePage />
+      <Users />
+      <CheckIn />
     </>
   );
 }
