750f21d1cd4157dfbd3cef7fe68c84f383b11aaa
nakfl <o0liebeo0o@gmail.com>
2020-07-15 12:52:00 +0700

    Add filter

--- src/containers/RoomPage/index.js
+++ src/containers/RoomPage/index.js
@@ -3,13 +3,14 @@ import Square from '../../components/Square';
 import axios from 'axios';
 import createPage from '../../components/createPage';
 import { BOOKING_PAGE } from '../../components/Sidebar/constants';
-import { Row, Col } from 'antd';
+import { Row, Col, Radio } from 'antd';
 
 const URL = process.env.SERV_HOST || 'http://localhost:8000';
 class RoomList extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      filter: null,
       listRoom: [],
     };
   }
@@ -24,9 +25,14 @@ class RoomList extends Component {
     });
   };
 
-  renderListRoom = (list) => {
+  componentDidMount = () => {
+    this.updateRoom();
+  };
+
+  renderListRoom = (list, filter) => {
+    if (filter !== null) list = list.filter((room) => room.status === filter);
     const rows = list.map((room) => (
-      <Col style={{ padding: '15px' }} span={4}>
+      <Col key={room.id} style={{ padding: '15px' }} span={4}>
         <Square
           {...room}
           key={room.id}
@@ -37,13 +43,32 @@ class RoomList extends Component {
     return <Row align='middle'>{rows}</Row>;
   };
 
-  componentDidMount = () => {
-    this.updateRoom();
+  handleOnChangeRadio = (e) => {
+    const { value } = e.target;
+    this.setState({
+      filter: value,
+    });
   };
 
   render() {
-    const { listRoom } = this.state;
-    return <div>{this.renderListRoom(listRoom)}</div>;
+    const { listRoom, filter } = this.state;
+    return (
+      <>
+        <Radio.Group
+          onChange={this.handleOnChangeRadio}
+          defaultValue={null}
+          buttonStyle='solid'
+          style={{ marginBottom: '15px' }}
+        >
+          <Radio.Button value={null}>All</Radio.Button>
+          <Radio.Button value={0}>Available</Radio.Button>
+          <Radio.Button value={1}>Rent</Radio.Button>
+          <Radio.Button value={2}>Reserved</Radio.Button>
+          <Radio.Button value={3}>Cleaning</Radio.Button>
+        </Radio.Group>
+        {this.renderListRoom(listRoom, filter)}
+      </>
+    );
   }
 }
 
