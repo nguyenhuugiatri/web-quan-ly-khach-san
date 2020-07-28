395c3f4e6d26fb98bc55b642a176670799ec2c38
HoaiBao <55429274+Hoai-Bao@users.noreply.github.com>
2020-07-28 16:54:44 +0700

    User control

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -9,8 +9,11 @@ module.exports = {
     return rows[0];
   },
   getAllUser: async () => {
-    const rows = await db.find('User');
+    const rows = await db.load(`select * from user where isDelete=0`);
     if (rows.length === 0) return null;
     return rows;
   },
+  resetPassword:(id,newPassword)=>db.load(`update user set password="${newPassword}" where id=${id}`),
+  delete:id=>db.load(`update user set isDelete =1 where id=${id}`),
+  update:(id,{username, permission})=>db.load(`update user set username='${username}', permission=${permission} where id =${id}`)
 };
