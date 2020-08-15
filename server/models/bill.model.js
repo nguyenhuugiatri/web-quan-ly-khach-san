1de9ebd5209ad4ae65601a71657a80125fd379d9
nakfl <o0liebeo0o@gmail.com>
2020-08-15 21:08:44 +0700

    Add service charge and room charge in bill table

--- server/models/bill.model.js
+++ server/models/bill.model.js
@@ -1,10 +1,16 @@
 const db = require('../database');
 
 module.exports = {
-  createNewBill: async (idRentReceipt, idUser, paymentDate, total) => {
+  createNewBill: async (
+    idRentReceipt,
+    idUser,
+    paymentDate,
+    roomCharge,
+    serviceCharge
+  ) => {
     await db.load(
-      `INSERT INTO Bill (idRentReceipt, idUser, paymentDate, total)
-      VALUES ('${idRentReceipt}', '${idUser}', '${paymentDate}', '${total}');
+      `INSERT INTO Bill (idRentReceipt, idUser, paymentDate, roomCharge, serviceCharge)
+      VALUES ('${idRentReceipt}', '${idUser}', '${paymentDate}', '${roomCharge}','${serviceCharge}');
       `
     );
   },
